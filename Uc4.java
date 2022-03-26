91ccb40925fb6ee18bef3cf692a7a70a88b99e6d
barbadeadesh <barbadeadesh2020@email.com>
2022-03-26 07:19:41 +0530

    find the max string by using length() method

---
+++ Uc4.java
@@ -0,0 +1,18 @@
+package com.bridgelab.generics;
+
+public class LengthExample {
+
+	public static void main(String args[]){  
+		String s1="abcd";  
+		String s2="efgh";  
+	
+		
+		if(s1.length()>s2.length()) {
+			
+			System.out.println("abcd");
+		}
+		else {
+			System.out.println("efgh");
+		}
+		
+}}
